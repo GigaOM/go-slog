218421d03b73dd4c9a8ee898195bc6a3024c5d6e
Matthew Batchelder <borkweb@gmail.com>
2012-10-23 15:53:52 -0400

    Don't add a local pointer to the DB singleton.
    I also took the liberty of alphabetizing this class.

--- components/class-go-slog.php
+++ components/class-go-slog.php
@@ -4,8 +4,7 @@ class Go_Slog
 {
 	public static $log_on = TRUE;
 	public static $config = array();
-	public static $simple_db;
-	
+
 	/**
 	 * constructor to setup the simple log
 	 * @param $aws_access_key string The Amazon Web Services access key
@@ -20,30 +19,16 @@ class Go_Slog
 		} // end if
 
 		static::$config = $config;
-		
+
 		if ( is_admin() )
 		{
 			require_once __DIR__ . '/class-go-slog-admin.php';
 			new Go_Slog_Admin();
 		} // end if
-		
+
 		add_filter( 'go_slog', 'Go_Slog::log', 10, 3 );
 	} // end function __construct
-	
-	/**
-	 * Setup and return an AWS SimpleDB Object, act as a singleton
-	 * @return SimpleDB object
-	 */
-	public static function simple_db()
-	{
-		if ( ! is_object( static::$simple_db ) )
-		{
-			static::$simple_db = Go_Simple_DB::get( static::$config['aws_sdb_domain'], static::$config['aws_access_key'], static::$config['aws_secret_key'] );
-		} // end if
-		
-		return static::$simple_db;
-	} // end function go_get_simple_db
-	
+
 	/**
 	 * log to SimpleDB
 	 * @param $code string The code you want to log
@@ -53,13 +38,22 @@ class Go_Slog
 	public static function log( $code = '', $message = '', $data = '' )
 	{
 		$microtime = explode( ' ', microtime() );
-				
+
 		$log_item['log_date'] = array( 'value' => $microtime[1] . substr( $microtime[0], 1 ) );
 		$log_item['host'] = array( 'value' => parse_url( site_url('/') , PHP_URL_HOST ) );
 		$log_item['code'] = array( 'value' => $code );
 		$log_item['message'] = array( 'value' => $message );
 		$log_item['data'] = array( 'value' => serialize( $data ) );
-		
+
 		static::simple_db()->putAttributes( static::$config['aws_sdb_domain'], uniqid( 'log_item_' ), $log_item );
 	} // end function log
+
+	/**
+	 * Return an AWS SimpleDB Object, leveraging the GO_Simple_DB::get singleton function
+	 * @return SimpleDB object
+	 */
+	public static function simple_db()
+	{
+		return GO_Simple_DB::get( static::$config['aws_sdb_domain'], static::$config['aws_access_key'], static::$config['aws_secret_key'] );
+	} // end function simple_db
 } // end class
