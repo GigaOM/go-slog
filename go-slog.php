704cb0dbe10227706b70dd82bc64414df8999544
Casey Bisson <casey.bisson@gmail.com>
2014-09-22 07:45:48 -0700

    Update go-slog.php

--- go-slog.php
+++ go-slog.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin name: Gigaom Simple DB Log
- * Description:	A way to log occurances to loggly and view them within WordPress.
+ * Description:	A simple way to log events, exceptions, anyting to Loggly and view them within WordPress.
  * Version:     2.1
  * Author: 		Gigaom
  * Author URI: 	http://gigaom.com/
