d03e85f12a646ed74a5fc1da3720aef6f4323fc5
Matthew Batchelder <borkweb@gmail.com>
2015-03-19 13:19:58 -0400

    Update go-slog.php

--- go-slog.php
+++ go-slog.php
@@ -5,6 +5,8 @@
  * Version:     2.1
  * Author: 		Gigaom
  * Author URI: 	http://gigaom.com/
+ * License: GPLv2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 require_once __DIR__ . '/components/class-go-slog.php';
